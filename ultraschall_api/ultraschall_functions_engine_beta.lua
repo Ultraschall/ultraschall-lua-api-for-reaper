5136ad7a3a524dae037d1b3f0f10b9c4cfee0d90
Meo Mespotine <lspmp3@yahoo.de>
2019-06-01 19:00:52 +0200

    FX-Management, like (re-)scanning plugins
    Build: 39160
    
    Additions:
        - FXManagement: AutoDetectVSTPlugins - auto-searches for the vst-plugin-folder
        - FXManagement: ScanDXPlugins - rescans all DX(DirectX)-plugins, optionally scans all or just the new ones
        - FXManagement: ScanVSTPlugins - rescans all vst-plugins, optionally clears vst-plugin-cache first
        - ReaMote: AutoSearchReaMoteSlaves - autosearches for new ReaMote-slaves

--- ultraschall_api/ultraschall_functions_engine_beta.lua
+++ ultraschall_api/ultraschall_functions_engine_beta.lua
@@ -25,7 +25,6 @@
 ]] 
 
 
-
 if type(ultraschall)~="table" then 
   -- update buildnumber and add ultraschall as a table, when programming within this file
   local retval, string = reaper.BR_Win32_GetPrivateProfileString("Ultraschall-Api-Build", "Functions-Build", "", reaper.GetResourcePath().."/UserPlugins/ultraschall_api/IniFiles/ultraschall_api.ini")
@@ -744,5 +743,218 @@ end
 
 --A,B,C,D=ultraschall.GetUserInputs("I got you", {"ShalalalaOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOHAH"}, {"HHHAAAAHHHHHHHHHHHHHHHHHHHHHHHHAHHHHHHHA"}, -1)
 
+function ultraschall.ScanVSTPlugins(clear_cache)
+--[[
+<US_DocBloc version="1.0" spok_lang="en" prog_lang="*">
+  <slug>ScanVSTPlugins</slug>
+  <requires>
+    Ultraschall=4.00
+    Reaper=5.977
+    SWS=2.10.0.1
+    JS=0.986
+    Lua=5.3
+  </requires>
+  <functioncall>ultraschall.ScanVSTPlugins(optional boolean clear_cache)</functioncall>
+  <description>
+    Re-scans all VST-Plugins.
+  </description>
+  <parameters>
+    optional boolean clear_cache - true, clear cache before re-scanning; false or nil, just scan vts-plugins
+  </parameters>
+  <chapter_context>
+    FX-Management
+    Plugins
+  </chapter_context>
+  <target_document>US_Api_Documentation</target_document>
+  <source_document>ultraschall_functions_engine.lua</source_document>
+  <tags>fx-management, scan, plugins, vst</tags>
+</US_DocBloc>
+--]]
+  local hwnd, hwnd1, hwnd2, retval, prefspage, reopen, id
+  local use_prefspage=210
+  if clear_cache==true then id=1058 else id=1057 end
+  hwnd = ultraschall.GetPreferencesHWND()
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) reopen=true end
+  retval, prefspage = reaper.BR_Win32_GetPrivateProfileString("REAPER", "prefspage", "-1", reaper.get_ini_file())
+  reaper.ViewPrefs(use_prefspage, 0)
+  hwnd = ultraschall.GetPreferencesHWND()
+  hwnd1=reaper.JS_Window_FindChildByID(hwnd, 0)
+  hwnd2=reaper.JS_Window_FindChildByID(hwnd1, id)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONDOWN", 1,1,1,1)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONUP", 1,1,1,1)
+
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  retval = reaper.BR_Win32_WritePrivateProfileString("REAPER", "prefspage", prefspage, reaper.get_ini_file())
+  reaper.ViewPrefs(prefspage, 0) 
+
+  if reopen~=true then 
+    hwnd = ultraschall.GetPreferencesHWND() 
+    if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  end
+end
+
+--ultraschall.ScanVSTPlugins(true)
+
+function ultraschall.AutoDetectVSTPlugins()
+--[[
+<US_DocBloc version="1.0" spok_lang="en" prog_lang="*">
+  <slug>AutoDetectVSTPlugins</slug>
+  <requires>
+    Ultraschall=4.00
+    Reaper=5.977
+    SWS=2.10.0.1
+    JS=0.986
+    Lua=5.3
+  </requires>
+  <functioncall>ultraschall.AutoDetectVSTPlugins()</functioncall>
+  <description>
+    Auto-detects the vst-plugins-folder.
+  </description>
+  <chapter_context>
+    FX-Management
+    Plugins
+  </chapter_context>
+  <target_document>US_Api_Documentation</target_document>
+  <source_document>ultraschall_functions_engine.lua</source_document>
+  <tags>fx-management, path, folder, auto-detect, plugins, vst</tags>
+</US_DocBloc>
+--]]
+  local hwnd, hwnd1, hwnd2, retval, prefspage, reopen, id
+  local use_prefspage=210
+  id=1117
+  hwnd = ultraschall.GetPreferencesHWND()
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) reopen=true end
+  retval, prefspage = reaper.BR_Win32_GetPrivateProfileString("REAPER", "prefspage", "-1", reaper.get_ini_file())
+  reaper.ViewPrefs(use_prefspage, 0)
+  hwnd = ultraschall.GetPreferencesHWND()
+  hwnd1=reaper.JS_Window_FindChildByID(hwnd, 0)
+  hwnd2=reaper.JS_Window_FindChildByID(hwnd1, id)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONDOWN", 1,1,1,1)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONUP", 1,1,1,1)
+
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  retval = reaper.BR_Win32_WritePrivateProfileString("REAPER", "prefspage", prefspage, reaper.get_ini_file())
+  reaper.ViewPrefs(prefspage, 0) 
+
+  if reopen~=true then 
+    hwnd = ultraschall.GetPreferencesHWND() 
+    if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  end
+end
+
+--ultraschall.AutoDetectVSTPlugins()
+
+function ultraschall.ScanDXPlugins(re_scan)
+--[[
+<US_DocBloc version="1.0" spok_lang="en" prog_lang="*">
+  <slug>ScanDXPlugins</slug>
+  <requires>
+    Ultraschall=4.00
+    Reaper=5.977
+    SWS=2.10.0.1
+    JS=0.986
+    Lua=5.3
+  </requires>
+  <functioncall>ultraschall.ScanDXPlugins(optional boolean re_scan)</functioncall>
+  <description>
+    (Re-)scans all DX-Plugins.
+  </description>
+  <parameters>
+    optional boolean clear_cache - true, re-scan all DX-plugins; false or nil, only scan new DX-plugins
+  </parameters>
+  <chapter_context>
+    FX-Management
+    Plugins
+  </chapter_context>
+  <target_document>US_Api_Documentation</target_document>
+  <source_document>ultraschall_functions_engine.lua</source_document>
+  <tags>fx-management, scan, plugins, dx</tags>
+</US_DocBloc>
+--]]
+  local hwnd, hwnd1, hwnd2, retval, prefspage, reopen, id
+  local use_prefspage=209
+  if re_scan==true then id=1060 else id=1059 end
+  hwnd = ultraschall.GetPreferencesHWND()
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) reopen=true end
+  retval, prefspage = reaper.BR_Win32_GetPrivateProfileString("REAPER", "prefspage", "-1", reaper.get_ini_file())
+  reaper.ViewPrefs(use_prefspage, 0)
+  hwnd = ultraschall.GetPreferencesHWND()
+  hwnd1=reaper.JS_Window_FindChildByID(hwnd, 0)
+  hwnd2=reaper.JS_Window_FindChildByID(hwnd1, id)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONDOWN", 1,1,1,1)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONUP", 1,1,1,1)
+
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  retval = reaper.BR_Win32_WritePrivateProfileString("REAPER", "prefspage", prefspage, reaper.get_ini_file())
+  reaper.ViewPrefs(prefspage, 0) 
+
+  if reopen~=true then 
+    hwnd = ultraschall.GetPreferencesHWND() 
+    if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  end
+end
+
+--ultraschall.ScanDXPlugins()
+
+function ultraschall.AutoSearchReaMoteSlaves()
+--[[
+<US_DocBloc version="1.0" spok_lang="en" prog_lang="*">
+  <slug>AutoSearchReaMoteSlaves</slug>
+  <requires>
+    Ultraschall=4.00
+    Reaper=5.977
+    SWS=2.10.0.1
+    JS=0.986
+    Lua=5.3
+  </requires>
+  <functioncall>ultraschall.AutoSearchReaMoteSlaves()</functioncall>
+  <description>
+    Auto-searches for new ReaMote-Slaves
+  </description>
+  <chapter_context>
+    ReaMote
+  </chapter_context>
+  <target_document>US_Api_Documentation</target_document>
+  <source_document>ultraschall_functions_engine.lua</source_document>
+  <tags>reamote, scan, search, slaves</tags>
+</US_DocBloc>
+--]]
+  local hwnd, hwnd1, hwnd2, retval, prefspage, reopen, id
+  local use_prefspage=227
+  id=1076
+  hwnd = ultraschall.GetPreferencesHWND()
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) reopen=true end
+  retval, prefspage = reaper.BR_Win32_GetPrivateProfileString("REAPER", "prefspage", "-1", reaper.get_ini_file())
+  reaper.ViewPrefs(use_prefspage, 0)
+  hwnd = ultraschall.GetPreferencesHWND()
+  hwnd1=reaper.JS_Window_FindChildByID(hwnd, 0)
+  hwnd2=reaper.JS_Window_FindChildByID(hwnd1, id)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONDOWN", 1,1,1,1)
+  reaper.JS_WindowMessage_Send(hwnd2, "WM_LBUTTONUP", 1,1,1,1)
+
+  if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  retval = reaper.BR_Win32_WritePrivateProfileString("REAPER", "prefspage", prefspage, reaper.get_ini_file())
+  reaper.ViewPrefs(prefspage, 0) 
+
+  if reopen~=true then 
+    hwnd = ultraschall.GetPreferencesHWND() 
+    if hwnd~=nil then reaper.JS_Window_Destroy(hwnd) end
+  end
+end
+
+--ultraschall.AutoSearchReaMoteSlaves()
+
+
+--[[
+hwnd = ultraschall.GetPreferencesHWND()
+hwnd2 = reaper.JS_Window_FindChildByID(hwnd, 1110)
+
+--reaper.JS_Window_Move(hwnd2, 110,11)
+
+
+for i=-1000, 10 do
+  A,B,C,D=reaper.JS_WindowMessage_Post(hwnd2, "TVHT_ONITEM", i,i,i,i)
+end
+--]]
 
 ultraschall.ShowLastErrorMessage()
