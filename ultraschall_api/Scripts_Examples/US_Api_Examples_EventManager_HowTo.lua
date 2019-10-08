e1b68e0c732b9c5fd1c6b12bdc2b6e8557231006
Meo Mespotine <lspmp3@yahoo.de>
2019-10-08 02:08:49 +0200

    License-stuff
    MIT-licenses added to all Lua-scripts, who didn't have one yet.

--- ultraschall_api/Scripts_Examples/US_Api_Examples_EventManager_HowTo.lua
+++ ultraschall_api/Scripts_Examples/US_Api_Examples_EventManager_HowTo.lua
@@ -1,3 +1,30 @@
+  --[[
+  ################################################################################
+  # 
+  # Copyright (c) 2014-2019 Ultraschall (http://ultraschall.fm)
+  # 
+  # Permission is hereby granted, free of charge, to any person obtaining a copy
+  # of this software and associated documentation files (the "Software"), to deal
+  # in the Software without restriction, including without limitation the rights
+  # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  # copies of the Software, and to permit persons to whom the Software is
+  # furnished to do so, subject to the following conditions:
+  # 
+  # The above copyright notice and this permission notice shall be included in
+  # all copies or substantial portions of the Software.
+  # 
+  # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  # THE SOFTWARE.
+  # 
+  ################################################################################
+  --]]
+  
+  
 -- How to use the Ultraschall-API EventManager
 --
 -- Meo Mespotine - 1st of October 2019
@@ -37,10 +64,13 @@ EventIdentifier = ultraschall.EventManager_AddEvent(
                                                     --       returns true(false) or not(true)
             false,                                  -- shall the event be paused(true) or checked for right away(true)
             TransitionPlayToPlaypause,              -- the eventcheck-functionname, 
-            {"40157, 0"}                            -- a table, which hold all actions and their corresponding sections
-                                                    --       in this case action 40157 from section 0
+            {"40157, 0",                            -- a table, which hold all actions and their corresponding sections
+             "40171, 0"}                            --       in this case action 40157 from section 0 and 40171 from section 0
                                                     --       note: both must be written as string "40157, 0"
                                                     --             if you want to add numerous actions, you can write them like
                                                     --             {  "40157, 0", "40171,0"}, which would add a marker and open 
                                                     --                                        the input-markername-dialog
+                                                    --             
+                                                    --       ActionCommandIDs with an underscore at the beginning are allowed.
+                                                    --       like: _SWS_AWSELGROUPIFGROUP or _RS1234567890, etc
                               )
