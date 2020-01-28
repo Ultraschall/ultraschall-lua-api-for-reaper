d638e1e72f5ada1549aeb872ac18c86e000145ed
Meo Mespotine <lspmp3@yahoo.de>
2020-01-28 21:59:30 +0100

    Theme-functions
    Build: 42956
    
    Additions:
       - Themeing: GetAllThemeLayoutNames - returns all layout-names and values of the current theme
       - Themeing: GetAllThemeLayoutParameters - returns all theme-layout-parameter attributes of the current theme

--- ultraschall_api/ultraschall_functions_engine_beta.lua
+++ ultraschall_api/ultraschall_functions_engine_beta.lua
@@ -1584,5 +1584,111 @@ function ultraschall.QueryMIDIMessageNameByID(modifier, key)
   return value
 end
 
+function ultraschall.GetAllThemeLayoutNames()
+--[[
+<US_DocBloc version="1.0" spok_lang="en" prog_lang="*">
+  <slug>GetAllThemeLayoutNames</slug>
+  <requires>
+    Ultraschall=4.00
+    Reaper=6.02
+    Lua=5.3
+  </requires>
+  <functioncall>integer index, table ThemeLayoutNames= ultraschall.GetAllThemeLayoutNames()</functioncall>
+  <description markup_type="markdown" markup_version="1.0.1" indent="default">
+    returns all layout-names and values of the current theme
+    
+    the table ThemeLayoutNames is of the following format:
+    
+      ThemeLayoutNames[parameter_index]["layout section"] - the name of the layout-section of the parameter
+      ThemeLayoutNames[parameter_index]["value"] - the value of the parameter
+      ThemeLayoutNames[parameter_index]["description"] - the description of the parameter
+    
+    returns nil in case of an error
+  </description>
+  <retvals>
+    integer index - the number of theme-layout-parameters available
+    table ThemeLayoutParameters - a table with all theme-layout-parameter available in the current theme
+  </retvals>
+  <chapter_context>
+    Themeing
+  </chapter_context>
+  <target_document>US_Api_Documentation</target_document>
+  <source_document>ultraschall_functions_engine.lua</source_document>
+  <tags>theme management, get, all, parameters</tags>
+</US_DocBloc>
+]]
+  local Aretval=true
+  local Layoutnames={}
+  local i=1
+  while Aretval==true do
+    Layoutnames[i]={}
+    Aretval, Layoutnames[i]["layout section"] = reaper.ThemeLayout_GetLayout("seclist", i)
+    Aretval, Layoutnames[i]["value"] = reaper.ThemeLayout_GetLayout(Layoutnames[i]["layout section"], -1)
+    Aretval, Layoutnames[i]["description"] = reaper.ThemeLayout_GetLayout(Layoutnames[i]["layout section"], -2)
+    
+    i=i+1
+  end
+  table.remove(Layoutnames, i-1)
+  return i-2, Layoutnames
+end
+
+--A,B,C=ultraschall.GetAllThemeLayoutNames()
+
+--Q,R = reaper.ThemeLayout_GetLayout("trans", -1)
+
+function ultraschall.GetAllThemeLayoutParameters()
+--[[
+<US_DocBloc version="1.0" spok_lang="en" prog_lang="*">
+  <slug>GetAllThemeLayoutParameters</slug>
+  <requires>
+    Ultraschall=4.00
+    Reaper=6.02
+    Lua=5.3
+  </requires>
+  <functioncall>integer index, table ThemeLayoutParameters = ultraschall.GetAllThemeLayoutParameters()</functioncall>
+  <description markup_type="markdown" markup_version="1.0.1" indent="default">
+    returns all theme-layout-parameter attributes of the current theme
+    
+    the table ThemeLayoutParameters is of the following format:
+    
+      ThemeLayoutParameters[parameter_index]["name"] - the name of the parameter
+      ThemeLayoutParameters[parameter_index]["description"] - the description of the parameter
+      ThemeLayoutParameters[parameter_index]["value"] - the value of the parameter
+      ThemeLayoutParameters[parameter_index]["value default"] - the defult value of the parameter
+      ThemeLayoutParameters[parameter_index]["value min"] - the minimum value of the parameter
+      ThemeLayoutParameters[parameter_index]["value max"] - the maximum value of the parameter
+    
+    returns nil in case of an error
+  </description>
+  <retvals>
+    integer index - the number of theme-layout-parameters available
+    table ThemeLayoutParameters - a table with all theme-layout-parameter available in the current theme
+  </retvals>
+  <chapter_context>
+    Themeing
+  </chapter_context>
+  <target_document>US_Api_Documentation</target_document>
+  <source_document>ultraschall_functions_engine.lua</source_document>
+  <tags>theme management, get, all, parameters</tags>
+</US_DocBloc>
+]]
+  local i=1
+  local ParamsIdx ={}
+  
+  while reaper.ThemeLayout_GetParameter(i) ~= nil do
+    local tmp, desc, C, D, E, F = reaper.ThemeLayout_GetParameter(i)
+    ParamsIdx[i]={}
+    ParamsIdx[i]["name"]=tmp
+    ParamsIdx[i]["description"]=desc
+    ParamsIdx[i]["value"]=C
+    ParamsIdx[i]["value default"]=D
+    ParamsIdx[i]["value min"]=E
+    ParamsIdx[i]["value max"]=F
+    
+    i = i + 1
+  end
+  
+  return i-1, ParamsIdx
+end
 
 ultraschall.ShowLastErrorMessage()
