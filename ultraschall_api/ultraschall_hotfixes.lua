de4565c8be5246cd88d10fa410f879bd5beda269
Meo-Ada Mespotine <lspmp3@yahoo.de>
2020-07-21 05:49:18 +0200

    Final touches and done
    Build: 43234
    
    Changes:
    - Docs: Reaper Internals - updated to Reaper 6.13
       - Docs: render-codes - added codes, who are available after having FFMPEG 4.1.3 installed

--- ultraschall_api/ultraschall_hotfixes.lua
+++ ultraschall_api/ultraschall_hotfixes.lua
@@ -27,7 +27,9 @@
 -- THIS FILE DOESN'T WORK SOMEHOW RIGHT NOW, so post fixes please as issues for the moment: 
 -- https://github.com/Ultraschall/ultraschall-lua-api-for-reaper/
 -- Sorry for that...
-
+-- Note July 2020: 
+-- the functions are loaded correctly, but they'll be overwritten, when the module is loaded, as the function is in there too(but the old one)
+-- so the hotfixes-attempt doesn't work anymore, due the moduleloader-speed-improvements. Pity but well...
 
 -- This is the file for hotfixes of buggy functions.
 
