f6d71ffc3f0937b818316d818cd5fb9fa2e99a59
Meo-Ada Mespotine <lspmp3@yahoo.de>
2020-10-11 03:21:11 +0200

    Final touches and 4.1.006 is done
    Final touches and 4.1.006 is done

--- ultraschall_api/ultraschall_functions_engine.lua
+++ ultraschall_api/ultraschall_functions_engine.lua
@@ -211,7 +211,7 @@ function ultraschall.GetApiVersion()
 </US_DocBloc>
 --]]
   local retval, BuildNumber = reaper.BR_Win32_GetPrivateProfileString("Ultraschall-Api-Build", "API-Build", "", reaper.GetResourcePath().."/UserPlugins/ultraschall_api/IniFiles/ultraschall_api.ini")
-  return 410.006, "4.1","15th of October 2020", "006",  "\"Rage against the machines - Renegades of Funk\"", ultraschall.hotfixdate, BuildNumber
+  return 410.006, "4.1","11th of October 2020", "006",  "\"Rage against the machines - Renegades of Funk\"", ultraschall.hotfixdate, BuildNumber
 end
 
 --A,B,C,D,E,F,G,H,I=ultraschall.GetApiVersion()
