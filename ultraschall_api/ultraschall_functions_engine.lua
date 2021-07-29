45e4088076ad0cefb3308e88b024549bd6b137f6
Meo-Ada Mespotine <lspmp3@yahoo.de>
2021-07-29 23:48:01 +0200

    Update Changelog

--- ultraschall_api/ultraschall_functions_engine.lua
+++ ultraschall_api/ultraschall_functions_engine.lua
@@ -212,7 +212,7 @@ function ultraschall.GetApiVersion()
 </US_DocBloc>
 --]]
   local retval, BuildNumber = reaper.BR_Win32_GetPrivateProfileString("Ultraschall-Api-Build", "API-Build", "", reaper.GetResourcePath().."/UserPlugins/ultraschall_api/IniFiles/ultraschall_api.ini")
-  return 420.005, "4.2","14th of July 2021", "005",  "\"Anne Clark - Our Darkness\"", "xx of xxxx xxxx", BuildNumber..".00"
+  return 420.006, "4.2","xx of XXXX 2021", "006",  "\" - \"", "xx of xxxx xxxx", BuildNumber..".00"
 end
 
 --A,B,C,D,E,F,G,H,I=ultraschall.GetApiVersion()
