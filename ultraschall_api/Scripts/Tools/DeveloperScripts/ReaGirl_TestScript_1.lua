14cc205a1c4ac0ac8dc35ca4d582bcf226dde15a
Meo-Ada Mespotine <lspmp3@yahoo.de>
2024-04-28 21:52:48 +0200

    ReaGirl
    First demo-script for public beta test

--- ultraschall_api/Scripts/Tools/DeveloperScripts/ReaGirl_TestScript_1.lua
+++ ultraschall_api/Scripts/Tools/DeveloperScripts/ReaGirl_TestScript_1.lua
@@ -1,4 +1,77 @@
+function CheckForDependencies(ReaImGui, js_ReaScript, US_API, SWS, Osara)
+  if US_API==true or js_ReaScript==true or ReaImGui==true or SWS==true or Osara==true then
+    if US_API==true and reaper.file_exists(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")==false then
+      US_API="Ultraschall API" -- "Ultraschall API" or ""
+    else
+      US_API=""
+    end
+    
+    if reaper.JS_ReaScriptAPI_Version==nil and js_ReaScript==true then
+      js_ReaScript="js_ReaScript" -- "js_ReaScript" or ""
+    else
+      js_ReaScript=""
+    end
+    
+    if reaper.ImGui_GetVersion==nil and ReaImGui==true then
+      ReaImGui="ReaImGui" -- "ReaImGui" or ""
+    else
+      ReaImGui=""
+    end
+    
+    if reaper.CF_GetSWSVersion==nil and SWS==true then
+      SWS="SWS" -- "ReaImGui" or ""
+    else
+      SWS=""
+    end
+    
+    if reaper.osara_outputMessage==nil and Osara==true then
+      Osara="Osara" -- "ReaImGui" or ""
+    else
+      Osara=""
+    end
+    
+    if Osara=="" and SWS=="" and js_ReaScript=="" and ReaImGui=="" and US_API=="" then return true end
+    local state=reaper.MB("This script needs additionally \n\n"..ReaImGui.."\n"..js_ReaScript.."\n"..US_API.."\n"..SWS.."\n"..Osara.."\n\ninstalled to work. Do you want to install them?", "Dependencies required", 4) 
+    if state==7 then return false end
+    if SWS~="" then
+      reaper.MB("SWS can be downloaded from sws-extension.org/download/pre-release/", "SWS missing", 0)
+    end
+    
+    if Osara~="" then
+      reaper.MB("Osara can be downloaded from https://osara.reaperaccessibility.com/", "Osara missing", 0)
+    end
+    
+    if reaper.ReaPack_BrowsePackages==nil and (US_API~="" or ReaImGui~="" or js_ReaScript~="") then
+      reaper.MB("Some uninstalled dependencies need ReaPack to be installed. Can be downloaded from https://reapack.com/", "ReaPack missing", 0)
+      return false
+    else
+      
+      if US_API=="Ultraschall API" then
+        reaper.ReaPack_AddSetRepository("Ultraschall API", "https://github.com/Ultraschall/ultraschall-lua-api-for-reaper/raw/master/ultraschall_api_index.xml", true, 2)
+        reaper.ReaPack_ProcessQueue(true)
+      end
+      
+      if US_API~="" or ReaImGui~="" or js_ReaScript~="" then 
+        reaper.ReaPack_BrowsePackages(js_ReaScript.." OR "..ReaImGui.." OR "..US_API)
+      end
+    end
+  end
+  return true
+end
+
+state=CheckForDependencies(false, true, false, true, true)
+if state==false then reaper.MB("Can't start script due to missing dependencies", "Error", 0) return end
+if reaper.JS_ReaScriptAPI_Version==nil then return end
+
 dofile(reaper.GetResourcePath().."/UserPlugins/reagirl.lua")
+
+function button(element_id)
+  if butt1==element_id then
+    reaper.MB("First button pressed", "Button pressed", 0)
+  elseif butt2==element_id then
+    reaper.MB("Second button pressed", "Button pressed", 0)
+  end
+end
 
 reagirl.Gui_New()
 
@@ -18,7 +91,11 @@ reagirl.DropDownMenu_Add(nil,nil, -20,"Drop Down Menu", 100, "A test Drop Down M
 reagirl.NextLine()
 reagirl.Inputbox_Add(nil, nil, -20, "An input box:", 100, "Input some text in here.", "", nil, nil)
 
-reagirl.Gui_Open("ReaGirl Testdialog #1", false, "ReaGirl Testdialog #1", "a test dialog that features all available ui-elements.", 355, 435, nil, nil, nil)
+reagirl.NextLine(5)
+butt1=reagirl.Button_Add(nil, nil, 0, 0, "Button #1", "The first button.", button)
+butt2=reagirl.Button_Add(nil, nil, 0, 0, "Button #2", "The second button.", button)
+
+reagirl.Gui_Open("ReaGirl Testdialog #1", false, "ReaGirl Testdialog #1", "a test dialog that features all available ui-elements.", 355, 165, nil, nil, nil)
 
 function main()
   reagirl.Gui_Manage()
