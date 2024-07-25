a72eb4fdeee33b26483d797a89e00c816993ead3
Meo-Ada Mespotine <lspmp3@yahoo.de>
2024-07-25 21:39:54 +0200

    updated release date

--- ultraschall_api/ultraschall_functions_engine.lua
+++ ultraschall_api/ultraschall_functions_engine.lua
@@ -190,7 +190,7 @@ function ultraschall.GetApiVersion()
 </US_DocBloc>
 --]]
   local retval, BuildNumber = reaper.BR_Win32_GetPrivateProfileString("Ultraschall-Api-Build", "API-Build", "", ultraschall.Api_Path.."IniFiles/ultraschall_api.ini")
-  return 500, "5","22nd of May 2024", "",  "\"Eels - Novocaine for the Soul\"", "xx of xxxx xxxx", BuildNumber..".00"
+  return 500, "5","9th of August 2024", "",  "\"Eels - Novocaine for the Soul\"", "xx of xxxx xxxx", BuildNumber..".00"
 end
 
 --A,B,C,D,E,F,G,H,I=ultraschall.GetApiVersion()
